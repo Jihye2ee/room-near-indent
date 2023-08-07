c7df5cc1161d7d79ae0d235f52038064c386b9de
Lee Jihye <0516ja@gmail.com>
2023-08-08 03:15:33 +0900

    main 페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,47 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import { Button, ToggleButton } from '@/src/ui/atoms'
+import { ToggleButtonGroup } from '@/src/ui/mui'
+import { Fade, Stack, Typography } from '@mui/material'
 
 export default function Home() {
+  const [type, setType] = useState<string>('')
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Stack alignItems='center' justifyContent='center' height='100%'>
+      <Typography sx={{ fontSize: '20px', fontWeight: 500, lineHeight: '20px' }} tabIndex={0} aria-label='회사 근처 집을 찾고 계신가요'>회사 근처 🏠을 찾고 계신가요?</Typography>
+      <Typography sx={{ fontSize: '16px', fontWeight: 500, my: 2 }}>👇</Typography>
+      <Stack>
+        <Fade in={true} timeout={1000}>
+          <Stack>
+            <ToggleButtonGroup
+              value={type}
+              exclusive
+              sx={{
+                mb: 2,
+                width: '300px',
+              }}
+              onChange={(_, value: string) => setType(value)}
+              aria-label='원하는 타입 선택'
+            >
+              <ToggleButton value='officetel' aria-label='오피스텔' sx={{ width: '100%'  }}>
+                <Typography variant='body2'>오피스텔</Typography>
+              </ToggleButton>
+              <ToggleButton value='villa' aria-label='빌라, 투룸+' sx={{ width: '100%' }}>
+                <Typography variant='body2'>빌라, 투룸+</Typography>
+              </ToggleButton>
+              <ToggleButton value='oneroom' aria-label='원룸' sx={{ width: '100%' }}>
+                <Typography variant='body2'>원룸</Typography>
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <Link href={`/${type ?? 'officetel'}`}>
+              <Button role='button' sx={{ width: '100%' }}>집 찾기 go</Button>
+            </Link>
+          </Stack>
+        </Fade>
+      </Stack>
+    </Stack>
   )
 }
