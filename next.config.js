c8c2759105c3f7b8faf85ea2d7173914f99d92cc
Lee Jihye <0516ja@gmail.com>
2023-08-08 02:52:55 +0900

    프로젝트 패캐지 설치 및 설정 파일 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  images: {
+    imageSizes: [64, 96, 128],
+    deviceSizes: [640, 768, 1024, 1200],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
