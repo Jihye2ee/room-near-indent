1cb807763bd4f1e155247f42bf4cffe8681a3f6f
Lee Jihye <0516ja@gmail.com>
2023-08-16 01:04:27 +0900

    네이버부동산 매물 검색 쿼리 추가

---
+++ src/data/naverland/queries.ts
@@ -0,0 +1,99 @@
+import queryString from 'query-string'
+
+import { State } from '@/src/ui/components/Conditions'
+
+import { NaverlandAddressItem } from '../address/types'
+
+export const getNaverLandCortarNo = async (coords: { x: number, y: number }): Promise<NaverlandAddressItem> => {
+  try {
+    const queryParams = queryString.stringify({
+      zoom: 16,
+      centerLat: coords.y,
+      centerLon: coords.x,
+    })
+    const naverURL = `https://new.land.naver.com/api/cortars?${queryParams}`
+    const url = `${process.env.NEXT_PUBLIC_WEB_BASE_URL}/api/naverland?url=${encodeURIComponent(naverURL)}`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch(error) {
+    throw error
+  }
+}
+
+export const getNaverLandList = async (item: NaverlandAddressItem, state: State) => {
+  try {
+    const queryParams = queryString.stringify({
+      cortarNo: item.cortarNo,
+      zoom: 16,
+      priceType: 'RETAIL',
+      markerId: '',
+      markerType: '',
+      selectedComplexNo: '',
+      selectedComplexBuildingNo: '',
+      fakeComplexMarker: '',
+      realEstateType: 'OPST',
+      tradeType: state.type === 'deposit' ? 'B1' : 'B2',
+      tag: '::::::::',
+      rentPriceMin: state.deposit[0],
+      rentPriceMax: state.deposit[1],
+      priceMin: 0,
+      priceMax: 900000000,
+      areaMin: 0,
+      areaMax: 900000000,
+      oldBuildYears: '',
+      recentlyBuildYears: '',
+      minHouseHoldCount: '',
+      maxHouseHoldCount: '',
+      showArticle: false,
+      sameAddressGroup: false,
+      minMaintenanceCost: '',
+      maxMaintenanceCost: '',
+      directions: '',
+      leftLon: state.area.bounds.leftLon,
+      rightLon: state.area.bounds.rightLon,
+      topLat: state.area.bounds.topLat,
+      bottomLat: state.area.bounds.bottomLat,
+      isPresale: true
+    })
+    const naverURL = `https://new.land.naver.com/api/complexes/single-markers/2.0?${queryParams}`
+    const url = `${process.env.NEXT_PUBLIC_WEB_BASE_URL}/api/naverland?url=${encodeURIComponent(naverURL)}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+  } catch {
+    return []
+  }
+}
+
+export const getNaverlandData = async (path: string, naverlandAddress: NaverlandAddressItem, state: State) => {
+  const params = queryString.stringify({
+    cortarNo: naverlandAddress.cortarNo,
+    order: 'rank',
+    realEstateType: path === 'officetel' ? 'OPST' : path === 'villa' ? 'VL' : 'APT:OPST:ABYG:OBYG:GM:OR:VL:DDDGG:JWJT:SGJT:HOJT',
+    tradeType: state.type === 'deposit' ? 'B1' : 'B2',
+    tag: path === 'oneroom' ? ':::::::SMALLSPCRENT:ONEROOM' : '::::::::',
+    rentPriceMin: state.rent[0],
+    rentPriceMax: state.rent[1],
+    priceMin: state.deposit[0],
+    priceMax: state.deposit[1],
+    areaMin: 0,
+    areaMax: 900000000,
+    showArticle: false,
+    sameAddressGroup: false,
+    priceType: 'RETAIL',
+    directions: '',
+    page: state.page ? state.page : 1,
+  })
+
+  const naverURL = `https://new.land.naver.com/api/articles?${params}`
+  const url = `${process.env.NEXT_PUBLIC_WEB_BASE_URL}/api/naverland?url=${encodeURIComponent(naverURL)}`
+  const response = await fetch(url)
+  const data = await response.json()
+
+  console.log('[data]', data)
+  return data
+
+}
--- src/data/types.ts
+++ src/data/types.ts
@@ -16,3 +16,61 @@ export type PropertyInfo = {
   size_m2: number
   title: string
 }
+
+export type NaverlandItem = {
+  articleList: Article[]
+  isMoreData: boolean
+  mapExposedCount: number
+  nonMapExposedIncluded: boolean
+}
+
+export type Article = {
+  articleNo: string
+  area1: string
+  area2: string
+  articleConfirmYmd: string
+  articleFeatureDesc: string
+  articleName: string
+  articleRealEstateTypeCode: string // "C03"
+  articleRealEstateTypeName: string // "빌라/연립"
+  articleStatus: string // R0"
+  buildingName: string
+  cpMobileArticleLinkUseAtArticleTitleYn: boolean
+  cpMobileArticleLinkUseAtCpNameYn: boolean
+  cpMobileArticleUrl: string
+  cpName: string
+  cpPcArticleBridgeUrl: string
+  cpPcArticleLinkUseAtArticleTitleYn: boolean
+  cpPcArticleLinkUseAtCpNameYn: boolean
+  cpPcArticleUrl: string
+  cpid: string
+  dealOrWarrantPrc: string
+  detailAddress: string
+  detailAddressYn: string
+  direction: string // "동향"
+  floorInfo: string // "2/4"
+  isComplex: boolean // false
+  isDirectTrade: boolean // false
+  isInterest: boolean // false
+  isLocationShow: boolean
+  isPriceModification: boolean
+  latitude: string // "37.597911"
+  longitude: string // "126.955946"
+  priceChangeState: string
+  realEstateTypeCode: string // "VL"
+  realEstateTypeName: string // "빌라"
+  realtorId: string // "dsds0824"
+  realtorName: string // "대성공인중개사사무소"
+  rentPrc: string // "40"
+  representativeImgThumb: string
+  representativeImgTypeCode: string // "10"
+  representativeImgUrl: string // "/20230812_22/land_naver_16918062668471EWrz_JPEG/temp_reg_image_1.jpg"
+  sameAddrCnt: number // 1
+  sameAddrDirectCnt: number // 0
+  sameAddrMaxPrc: string // "5,000/40"
+  sameAddrMinPrc: string // "5,000/40"
+  siteImageCount: number // 0
+  tradeTypeName: string
+  tradeTypeCode: string
+  tagList: string[]
+}
