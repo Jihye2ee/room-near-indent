abc95dfb850a9b51194e3dd837b2cf2d20ef2c89
Lee Jihye <0516ja@gmail.com>
2023-08-16 01:02:16 +0900

    state 관리를 위한 recoil 설치

---
+++ app/recoil-state.ts
@@ -0,0 +1,29 @@
+
+import { atom } from 'recoil'
+
+import { State } from '@/src/ui/components/Conditions'
+
+export const filterState = atom<State>({
+  key: 'filterState',
+  default: {
+    site: 'zigbang',
+    type: 'deposit',
+    deposit: [0, 40000],
+    rent: [0, 150],
+    area: {
+      address: null,
+      address_name: '',
+      address_type: '',
+      x: '',
+      y: '',
+      road_address: null,
+      bounds: {
+        bottomLat: '37.5298390',
+        leftLon: '127.0388926',
+        rightLon: '127.0751460',
+        topLat: '37.5520865',
+      }
+    },
+    page: 1,
+  }
+})
