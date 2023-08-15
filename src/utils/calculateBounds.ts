c8a5ea4ce68adf2c7ff6e1bf75d9592b212e4e74
Lee Jihye <0516ja@gmail.com>
2023-08-16 01:03:31 +0900

    box 위도, 경도 계산 유틸 추가

---
+++ src/utils/calculateBounds.ts
@@ -0,0 +1,13 @@
+import { Bounds } from '../data/address/types'
+
+export const calculateBounds = (centerLatitude: number, centerLongitude: number): Bounds => {
+  const deltaLat = 1.23 / 110.574 // 위도 1도는 약 110.574km
+  const deltaLon = 1.60 / (111.320 * Math.cos((centerLatitude * Math.PI) / 180))
+
+  return {
+    topLat: (centerLatitude + deltaLat).toFixed(7),
+    bottomLat: (centerLatitude - deltaLat).toFixed(7),
+    leftLon: (centerLongitude - deltaLon).toFixed(7),
+    rightLon: (centerLongitude + deltaLon).toFixed(7),
+  }
+}
