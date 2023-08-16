36caab3514b191f2dc4f774bf5d9453b215baa7e
Lee Jihye <0516ja@gmail.com>
2023-08-17 02:14:33 +0900

    bound 값 계산 조정

--- src/utils/calculateBounds.ts
+++ src/utils/calculateBounds.ts
@@ -1,8 +1,8 @@
 import { Bounds } from '../data/address/types'
 
 export const calculateBounds = (centerLatitude: number, centerLongitude: number): Bounds => {
-  const deltaLat = 1.23 / 110.574 // 위도 1도는 약 110.574km
-  const deltaLon = 1.60 / (111.320 * Math.cos((centerLatitude * Math.PI) / 180))
+  const deltaLat = 1.5 / 110.574 // 위도 1도는 약 110.574km
+  const deltaLon = 1.5 / (111.320 * Math.cos((centerLatitude * Math.PI) / 180))
 
   return {
     topLat: (centerLatitude + deltaLat).toFixed(7),
