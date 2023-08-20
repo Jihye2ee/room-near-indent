230e6b1b67f5b983535d619414ffbcd00126cce2
Lee Jihye <0516ja@gmail.com>
2023-08-21 02:57:10 +0900

    메인페이지 버튼 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useReducer } from 'react'
 
 import { Stack, Typography } from '@/src/ui/mui'
@@ -56,6 +56,7 @@ const reducer = (state: State, action: Action) => {
 }
 
 export default function Home() {
+  const router = useRouter()
   const [state, dispatch] = useReducer(reducer, { isCompanyEmojiShow: true, isSubwayEmojiShow: true, isSchoolEmojiShow: true, isZigbangEmojiShow: true, isNaverlandEmojiShow: true })
   return (
     <Stack alignItems='center' role='navigation' aria-label='주거 유형 선택' sx={{ mt: { laptop: 10, tablet: 5, mobile: 2 }}}>
@@ -87,23 +88,41 @@ export default function Home() {
         을 돌아다니지 않고, <br/>원하는 집을 탐색해보세요!
       </Typography>
       <Stack direction='row' justifyContent='center' alignItems='center' spacing={2} pl={2} pr={2} sx={{ width: { mobile: '100%' }}}>
-        <Stack borderRadius={2} justifyContent='center' alignItems='center' height={100} sx={{ backgroundColor: '#356EFB', width: { laptop: '120px', mobile: '100%' }}}>
-          <Link href='/officetel' style={{ textAlign: 'center' }}>
-            <BusinessIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.200' }} />
-            <Typography variant='body2' sx={{ color: 'grey.200', fontWeight: 600 }}>오피스텔</Typography>
-          </Link>
+        <Stack
+          component='button'
+          borderRadius={2}
+          justifyContent='center'
+          alignItems='center'
+          height={100}
+          sx={{ backgroundColor: '#356EFB', width: { laptop: '120px', mobile: '100%' }, cursor: 'pointer', border: 'none' }}
+          onClick={() => router.push('/officetel')}
+        >
+          <BusinessIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.100' }} />
+          <Typography variant='body2' sx={{ color: 'grey.100', fontWeight: 600 }}>오피스텔</Typography>
         </Stack>
-        <Stack borderRadius={2} justifyContent='center' alignItems='center' height={100} sx={{ backgroundColor: '#ffcb3b', width: { laptop: '120px', mobile: '100%' }}}>
-          <Link href='/villa' style={{ textAlign: 'center' }}>
-            <HomeIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.200' }} />
-            <Typography variant='body2' sx={{ color: 'grey.200', fontWeight: 600 }}>빌라, 투룸+</Typography>
-          </Link>
+        <Stack
+          component='button'
+          borderRadius={2}
+          justifyContent='center'
+          alignItems='center'
+          height={100}
+          sx={{ backgroundColor: '#F6CF01', width: { laptop: '120px', mobile: '100%' }, cursor: 'pointer', border: 'none' }}
+          onClick={() => router.push('/villa')}
+        >
+          <HomeIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.100' }} />
+          <Typography variant='body2' sx={{ color: 'grey.100', fontWeight: 600 }}>빌라, 투룸+</Typography>
         </Stack>
-        <Stack borderRadius={2} justifyContent='center' alignItems='center' height={100} sx={{ backgroundColor: '#6D24FF', width: { laptop: '120px', mobile: '100%' }}}>
-          <Link href='/oneroom' style={{ textAlign: 'center' }}>
-            <BedIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.200' }} />
-            <Typography variant='body2' sx={{ color: 'grey.200', fontWeight: 600 }}>원룸</Typography>
-          </Link>
+        <Stack
+          component='button'
+          borderRadius={2}
+          justifyContent='center'
+          alignItems='center'
+          height={100}
+          sx={{ backgroundColor: '#6D24FF', width: { laptop: '120px', mobile: '100%' }, cursor: 'pointer', border: 'none' }}
+          onClick={() => router.push('/oneroom')}
+        >
+          <BedIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.100' }} />
+          <Typography variant='body2' sx={{ color: 'grey.100', fontWeight: 600 }}>원룸</Typography>
         </Stack>
       </Stack>
     </Stack>
