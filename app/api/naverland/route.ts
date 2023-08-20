3df400c112d1cdf2fd25d6e6dbe73d268c411e4e
Lee Jihye <0516ja@gmail.com>
2023-08-20 21:47:39 +0900

    user-agent 값 변경

--- app/api/naverland/route.ts
+++ app/api/naverland/route.ts
@@ -18,7 +18,8 @@ export async function GET(req: NextRequest) {
       headers: {
         'Origin': 'm.land.naver.com',
         'Referer': 'https://m.land.naver.com/',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        // 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
       }
     })
 
