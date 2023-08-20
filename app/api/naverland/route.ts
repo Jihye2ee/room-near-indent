73d82197a94a88a7a605f42d85e982f04f5333b1
Lee Jihye <0516ja@gmail.com>
2023-08-20 21:13:16 +0900

    api 호출 header 정보 수정

--- app/api/naverland/route.ts
+++ app/api/naverland/route.ts
@@ -12,44 +12,30 @@ export async function GET(req: NextRequest) {
         status: 400
       })
     }
-    const decodedURL = decodeURIComponent(url)
-    console.log('[request url]:', url, decodedURL)
-    const response = await fetch(decodedURL, {
+
+    const response = await fetch(url, {
       method: 'GET',
       headers: {
-        "Origin": "new.land.naver.com",
-        // https://new.land.naver.com/complexes?ms=37.539817,127.056888,16&a=APT:OPST:PRE&b=B1&e=RETAIL&ad=true
-        "Referer": `https://new.land.naver.com/complexes/?ms=37.482968,127.0634,16&a=OPST&b=B1&e=RETAIL`,
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-        'Content-Type': 'application/json;charset=UTF-8',
-        'Connection': 'keep-alive',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Sec-Ch-Ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-        'Sec-Ch-Ua-Mobile': '?0',
-        'Sec-Ch-Ua-Platform': '"macOS"',
-        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NAVER_LAND_TOKEN}`
+        'Origin': 'm.land.naver.com',
+        'Referer': 'https://m.land.naver.com/',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
       }
     })
 
     if (isNil(response) || !response.ok) {
-      console.error('Network response was not ok', response.status, response.statusText)
+      console.log('Network response was not ok', response.status, response.statusText)
       return NextResponse.json({ error: `${response.status, response.statusText} 에러가 발생했습니다.` }, { status: 500 })
     }
 
     const contentType = response.headers.get('Content-Type')
-    console.log('[contentType]', contentType)
+    console.log('response header', response.headers)
+    console.log('response contentType:', contentType)
     if (contentType && contentType.includes('application/json')) {
       const data = await response.json()
       return NextResponse.json(data, { status: 200 })
     } else {
       const data = await response.text()
-      console.log('[Authorization]:', `Bearer ${process.env.NEXT_PUBLIC_NAVER_LAND_TOKEN}`)
-      console.log('[content type is not application/json]', response.status, response.statusText, response.redirected)
+      console.log('response status type: ', response.status, response.statusText, response.redirected)
       return NextResponse.json(data, { status: 500 })
     }
   } catch(error) {
