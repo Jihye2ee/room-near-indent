13fc66d655e01f5c1c3f3b07263b9306194635c4
Lee Jihye <0516ja@gmail.com>
2023-08-21 02:56:49 +0900

    헤더 네비게이션 추가

---
+++ src/ui/components/DrawerList.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { useRouter } from 'next/navigation'
+
+import { Box, Divider, List, ListItem, ListItemButton, Stack, Typography } from '@/src/ui/mui'
+import BedIcon from '@mui/icons-material/Bed'
+import BusinessIcon from '@mui/icons-material/Business'
+import CloseIcon from '@mui/icons-material/Close'
+import HomeIcon from '@mui/icons-material/Home'
+
+type Props = {
+  onClose: () => void
+}
+const DrawerList = ({ onClose }: Props) => {
+  const router = useRouter()
+  return (
+    <Box role='presentation' sx={{ width: 300 }} onClick={onClose}>
+      <List sx={listStyleProps}>
+        <ListItem>
+          <ListItemButton onClick={() => router.push('/')}>
+            <img src='/logo.png' width={30} height={30} />
+            <Typography variant='body2' sx={{ color: 'grey.800', fontWeight: 600, ml: 1.5 }}>FindRoomNearBy</Typography>
+          </ListItemButton>
+          <Stack
+            aria-label='닫기'
+            sx={{ cursor: 'pointer' }}
+            onClick={onClose}
+          >
+            <CloseIcon sx={{ color: 'grey.800', mr: 1, fontSize: '1.5rem' }} />
+          </Stack>
+        </ListItem>
+        <Divider />
+        <ListItem sx={{ mt: 0.5 }}>
+          <ListItemButton onClick={() => router.push('/officetel')}>
+            <BusinessIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.600' }} />
+            <Typography variant='body2' sx={{ color: 'grey.600', fontWeight: 600, ml: 1.5 }}>오피스텔</Typography>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton onClick={() => router.push('/villa')}>
+            <HomeIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.600' }} />
+            <Typography variant='body2' sx={{ color: 'grey.600', fontWeight: 600, ml: 1.5 }}>빌라, 투룸+</Typography>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton onClick={() => router.push('/oneroom')}>
+            <BedIcon aria-hidden='true' sx={{ fontSize: '30px', color: 'grey.600' }} />
+            <Typography variant='body2' sx={{ color: 'grey.600', fontWeight: 600, ml: 1.5 }}>원룸</Typography>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  )
+}
+
+export default DrawerList
+
+const listStyleProps = {
+  '.MuiListItem-root': {
+    paddingRight: 0,
+    paddingLeft: 0
+  }
+}
--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,13 +1,92 @@
-import { Stack } from '@/src/ui/mui'
+'use client'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
+import { Drawer, Stack, SxProps, Typography } from '@/src/ui/mui'
+import MenuIcon from '@mui/icons-material/Menu'
+
+import DrawerList from './DrawerList'
 import ThemeToggle from './ThemeToggle'
 
 const Header = () => {
+  const router = useRouter()
+  const [open, setOpen] = useState(false)
   return (
-    <Stack p={2}>
-      <ThemeToggle />
-    </Stack>
+    <nav aria-label='Main Navigation' style={{ position: 'sticky', top: 0 }}>
+      <Stack direction='row' flexWrap='wrap' alignItems='center' justifyContent='space-between' width='100%' p={1.5} sx={{ backgroundColor: 'grey.100' }}>
+        <Stack direction='row' alignItems='center' spacing={3} sx={{ display: { laptop: 'flex', mobile: 'none' } }}>
+          <Stack
+            aria-label='Home'
+            component='button'
+            p={1}
+            sx={{ border: 'none', textAlign: 'center',  cursor: 'pointer', backgroundColor: 'grey.100' }}
+            onClick={() => router.push('/')}
+          >
+            <img src='/logo.png' width={30} height={30} alt='logo' />
+          </Stack>
+          <Stack
+            aria-label='Officetel'
+            component='button'
+            p={1}
+            sx={{ border: 'none', textAlign: 'center', cursor: 'pointer', backgroundColor: 'grey.100' }}
+            onClick={() => router.push('/officetel')}
+          >
+            <Typography sx={menuName}>오피스텔</Typography>
+          </Stack>
+          <Stack
+            aria-label='villa'
+            component='button'
+            p={1}
+            sx={{ border: 'none', textAlign: 'center', cursor: 'pointer', backgroundColor: 'grey.100' }}
+            onClick={() => router.push('/villa')}
+          >
+            <Typography sx={menuName}>빌라, 투룸+</Typography>
+          </Stack>
+          <Stack
+            aria-label='oneroom'
+            component='button'
+            p={1}
+            sx={{ border: 'none', textAlign: 'center', cursor: 'pointer', backgroundColor: 'grey.100' }}
+            onClick={() => router.push('/oneroom')}
+          >
+            <Typography sx={menuName}>원룸</Typography>
+          </Stack>
+        </Stack>
+        <Stack direction='row' alignItems='center' spacing={1} sx={{ display: { laptop: 'none', mobile: 'flex' } }}>
+          <Stack
+            aria-label='메뉴 열기'
+            component='button'
+            p={1}
+            sx={{ border: 'none', textAlign: 'center',  cursor: 'pointer', backgroundColor: 'grey.100' }}
+            onClick={() => setOpen(true)}
+          >
+            <MenuIcon aria-hidden='true' width={30} height={30} sx={{ color: 'grey.800' }} />
+          </Stack>
+        </Stack>
+        <ThemeToggle />
+        <Drawer
+          anchor='left'
+          open={open}
+          onClose={() => setOpen(!open)}
+          sx={drawerStyleProp}
+        >
+          <DrawerList onClose={() => setOpen(false)}/>
+        </Drawer>
+      </Stack>
+    </nav>
   )
 }
 
 export default Header
+
+const menuName: SxProps = {
+  fontSize: '1rem',
+  fontWeight: 600,
+  color: 'grey.600',
+}
+
+const drawerStyleProp: SxProps = {
+  '.css-10cyd05-MuiPaper-root-MuiDrawer-paper': {
+    backgroundColor: 'grey.200',
+  }
+}
