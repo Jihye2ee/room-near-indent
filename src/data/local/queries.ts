ae2b2ab93cb4ead8fadd2d6cef3e48fbb8530b17
Lee Jihye <0516ja@gmail.com>
2023-08-25 18:01:53 +0900

    조회

--- src/data/local/queries.ts
+++ src/data/local/queries.ts
@@ -1,12 +1,14 @@
 import queryString from 'query-string'
 
+import { KeywordQueryParams } from './types'
+
 export const getCortarsInfo = async (coords: { x: string, y: string }) => {
   const queryParams = queryString.stringify({
     x: Number(coords.x).toFixed(6),
     y: Number(coords.y).toFixed(6)
   })
-  const url = `https://dapi.kakao.com/v2/local/geo/coord2regioncode?${queryParams}`
-  const response = await fetch(url, {
+  const requestURL = `https://dapi.kakao.com/v2/local/geo/coord2regioncode?${queryParams}`
+  const response = await fetch(requestURL, {
     headers: {
       Authorization: `KakaoAK ${process.env.KAKAO_LOCAL_REST_API_KEY}`
     }
@@ -16,5 +18,39 @@ export const getCortarsInfo = async (coords: { x: string, y: string }) => {
   }
 
   const data = await response.json()
+  return data
+}
+
+export const getKakaoAddressSearch = async (query: string) => {
+  const queryParams = queryString.stringify({
+    query
+  })
+  const requestURL = `https://dapi.kakao.com/v2/local/search/address?${queryParams}`
+  const response = await fetch(requestURL, {
+    headers: {
+      Authorization: `KakaoAK ${process.env.KAKAO_LOCAL_REST_API_KEY}`
+    }
+  })
+  if (!response.ok) {
+    throw new Error()
+  }
+
+  const data = await response.json()
+  return data
+}
+
+export const getKakaoKeywordSearch = async (queryParams: KeywordQueryParams) => {
+  const params = queryString.stringify(queryParams)
+  const requestURL = `https://dapi.kakao.com/v2/local/search/keyword?${params}`
+  const response = await fetch(requestURL, {
+    headers: {
+      Authorization: `KakaoAK ${process.env.KAKAO_LOCAL_REST_API_KEY}`
+    }
+  })
+  if (!response.ok) {
+    throw new Error()
+  }
+
+  const data = response.json()
   return data
 }
