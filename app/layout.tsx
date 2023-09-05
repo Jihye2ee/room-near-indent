a95d06ed99eff50d0e63e3863be055076755eb33
Lee Jihye <0516ja@gmail.com>
2023-09-05 14:53:28 +0900

    검색결과에 favicon 나올 수 있도록 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
       </head>
       <body className={pretendard.className}>
         <script dangerouslySetInnerHTML={{ __html: ColorModeScript }} />
