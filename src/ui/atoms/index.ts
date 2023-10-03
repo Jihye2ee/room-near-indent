729c4f5662d8239f60484386c7650eba936fb87c
Lee Jihye <0516ja@gmail.com>
2023-10-03 21:20:58 +0900

    사용하지 않는 mui component 스타일 커스텀 정의 삭제

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,5 +1,3 @@
 'use client'
-export { default as ToggleButton } from './ToggleButton'
-export { default as Button } from './Button'
 export { default as Slider } from './Slider'
 export { default as TextFieldInput } from './TextFieldInput'
