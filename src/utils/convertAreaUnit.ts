0adbbfd0be70c4515a81f5f1e49115225ebfcced
Lee Jihye <0516ja@gmail.com>
2023-10-26 02:06:37 +0900

    평 <-> m2 변환하는 유틸 추가 및 수정

--- src/utils/convertAreaUnit.ts
+++ src/utils/convertAreaUnit.ts
@@ -1,7 +1,20 @@
 export const m2ToPyeong = (m2: number): string => {
-  return Math.floor(m2 / 3.3).toFixed(2)
+  return (m2 / 3.3).toFixed(2)
 }
 
 export const pyeonToM2 = (pyeong: number): string => {
-  return Math.floor(pyeong * 3.3).toFixed(2)
+  return (pyeong * 3.3).toFixed(2)
+}
+
+export const extractM2andConvertToPyeong = (text: string): string => {
+  const regex = /(\d+\.\d+)\/(\d+\.\d+)㎡/
+  const match = text.match(regex)
+
+  if (match) {
+    const value1 = match[1]
+    const value2 = match[2]
+    return `${m2ToPyeong(Number(value1))}/${m2ToPyeong(Number(value2))}평`
+  }
+
+  return ''
 }
